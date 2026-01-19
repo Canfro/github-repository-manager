3f68a02a21efb6f6d9d1c3afedd3477cec3a3661
Franco Dalla Via Oliveros <francodvo@gmail.com>
2026-01-19 03:01:57 -0300

    First commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use std::path::PathBuf;
+
+enum SourceType {
+    GITHUB,
+}
+
+fn pull_source(source: &str, source_type: SourceType) {
+    todo!()
+}
+
+fn build_package(package_id: &str, source_code: PathBuf) {
+    todo!()
+}
+
+fn main() {
+    todo!()
+}
